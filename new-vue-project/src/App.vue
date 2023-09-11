d6e631b6018129bf3c9359391f09345cb85754b6
gaifiev <v.gaifiev@mail.ru>
2023-09-11 23:58:49 +0300

    add framework vue dz5

---
+++ new-vue-project/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div v-for="(product, index) in products" :key="index">
+      <ProductDetails :product="product"></ProductDetails>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductDetails from './components/ProductDetails.vue';
+
+export default {
+  components: {
+    ProductDetails,
+},
+  data() {
+    return {
+      products: [
+        {
+          name: 'Яблоко',
+          price: 10,
+          available: true
+        },
+        {
+          name: 'Лимон',
+          price: 9,
+          available: false
+        },
+        {
+          name: 'Слива',
+          price: 15,
+          available: true
+        },
+        {
+          name: 'Абрикос',
+          price: 50,
+          available: true
+        },
+        {
+          name: 'Банан',
+          price: 6,
+          available: true
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ new-vue-project/src/components/ProductDetails.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h1>{{ product.name }}</h1>
+      <p>{{ formattedPrice }}</p>
+      <p>{{ availabilityText }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      product: {
+        type: Object,
+        required: true,
+      }
+    },
+    computed: {
+      formattedPrice() {
+        // Функция для форматирования цены со знаком валюты
+        const formatter = new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD' // нужная валюта
+        });
+        return formatter.format(this.product.price);
+      },
+      availabilityText() {
+        return this.product.available ? 'Available' : 'Out of stock';
+      }
+    }
+  };
+  </script>
+  
