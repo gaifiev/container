2baeafda2cbceea92f4aff0eca5ba834171cdc0c
gaifiev <v.gaifiev@mail.ru>
2024-01-05 18:48:23 +0300

    add dz_2

--- advanced_js/dz_2/1.js
+++ advanced_js/dz_2/1.js
@@ -1,2 +1,100 @@
 "use strict";
 
+/*
+###Задание 1
+Необходимо создать класс Library. Конструктор класса, должен принимать начальный 
+список книг (массив) в качестве аргумента. Убедитесь, что предоставленный массив 
+не содержит дубликатов; в противном случае необходимо выбросить ошибку.
+
+1. Класс должен содержать приватное свойство #books, которое должно хранить 
+книги, переданные при создании объекта.
+
+2. Реализуйте геттер-функцию allBooks, которая возвращает текущий список книг.
+
+3. Реализуйте метод addBook(title), который позволяет добавлять книгу в список. 
+Если книга с таким названием уже существует в списке, выбросьте ошибку с 
+соответствующим сообщением.
+
+4. Реализуйте метод removeBook(title), который позволит удалять книгу из списка 
+по названию. Если книги с таким названием нет в списке, выбросьте ошибку с 
+соответствующим сообщением.
+
+5. Реализуйте метод hasBook(title), который будет проверять наличие книги в 
+библиотеке и возвращать true или false в зависимости от того, есть ли такая 
+книга в списке или нет.
+*/
+
+class Library {
+  #books = [];
+  constructor(books) {
+    checkDublicat(books);
+    this.#books = books;
+  }
+
+  // получение книги
+  getAllBooks() {
+    return this.#books;
+  }
+
+  // добавление книги
+  addBook(title) {
+    if (this.#books.includes(title)) {
+      throw new Error(`Книга ${title} существует в списке книг`);
+    }
+    this.#books.push(title);
+  }
+
+  // удаление книги
+  removeBook(title) {
+    if (this.checkTitle(title) === -1) {
+      throw new Error(`Книга ${title} отсутствует в списке книг`);
+    }
+    this.#books.splice(this.checkTitle(title), 1);
+  }
+
+  //  проверка на наличие книги
+  hasBook(title) {
+    if (this.checkTitle(title) !== -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  checkTitle(title) {
+    return this.#books.indexOf(title);
+  }
+}
+
+function checkDublicat(books) {
+  if (books.length !== new Set(books).size)
+    throw new Error("Список книг содержит дубликаты");
+}
+
+const books = new Library([
+  "Гарри Поттер",
+  "Война и мир",
+  "Мастер и Маргарита",
+  "Преступление и наказание",
+  "Собачье сердце",
+  // "Собачье сердце",
+]);
+
+console.log("Список книг:");
+console.log(books.getAllBooks());
+
+console.log("Добавление книги:");
+books.addBook("Отцы и дети");
+books.addBook("Темные силы");
+// books.addBook("Темные силы");
+console.log(books.getAllBooks());
+
+console.log("Удаление книги:");
+books.removeBook("Гарри Поттер");
+books.removeBook("Темные силы");
+// books.removeBook("Темные");
+console.log(books.getAllBooks());
+
+console.log("Проверка на наличие книги:");
+console.log(books.hasBook("Отцы и дети"));
+console.log(books.hasBook("Темные силы"));
--- advanced_js/dz_2/2.js
+++ advanced_js/dz_2/2.js
@@ -1,2 +1,116 @@
 "use strict";
 
+/*
+###Задание 2
+Вы разрабатываете систему отзывов для вашего веб-сайта. Пользователи могут 
+оставлять отзывы, но чтобы исключить слишком короткие или слишком длинные 
+сообщения, вы решаете установить ограничение, отзыв должен быть не менее 50 
+символов в длину и не более 500. В случае неверной длины, необходимо выводить 
+сообщение об ошибке, рядом с полем для ввода.
+
+Создайте HTML-структуру. 
+На странице должны отображаться товары, под каждым товаром должен быть список 
+отзывов на данный товар. Под каждым списком отзывов должна быть форма, где можно
+добавить отзыв для продукта.
+
+При добавлении отзыва, он должен отображаться на странице под предыдущими 
+отзывами, а не заменять их.
+Массив initialData должен использоваться для начальной загрузки данных 
+при запуске вашего приложения.
+
+Каждый отзыв должен иметь уникальное id, для упрощения, используем `Date.now()`.
+
+ВНИМАНИЕ! Если вы не проходили на курсе работу с DOM, то можно это задание не 
+делать, пока рано.
+*/
+
+const initialData = [
+  {
+    product: "Apple iPhone 13",
+    reviews: [
+      {
+        id: 1,
+        text: "Отличный телефон! Батарея держится долго.",
+      },
+      {
+        id: 2,
+        text: "Камера супер, фото выглядят просто потрясающе.",
+      },
+    ],
+  },
+  {
+    product: "Samsung Galaxy Z Fold 3",
+    reviews: [
+      {
+        id: 3,
+        text: "Интересный дизайн, но дорогой.",
+      },
+    ],
+  },
+  {
+    product: "Sony PlayStation 5",
+    reviews: [
+      {
+        id: 4,
+        text: "Люблю играть на PS5, графика на высоте.",
+      },
+    ],
+  },
+];
+
+let currentRecall;
+let currentIndexRecall = 0;
+const container = document.querySelector(".container");
+for (let i = 0; i < initialData.length; i++) {
+  let recall = ``;
+  let prodact = `<div id="${i}" class="prodact">
+  <h1>${initialData[i].product}</h1>`;
+  for (let j = 0; j < initialData[i].reviews.length; j++) {
+    currentIndexRecall += 1;
+    recall += `<p>Отзыв №${initialData[i].reviews[j].id}: ${initialData[i].reviews[j].text}</p>`;
+  }
+  prodact =
+    prodact +
+    '<div class="wrapper">' +
+    recall +
+    '<input type="text" class="currentRecall">' +
+    '<div class="error-message"></div>' +
+    '<button class="add-button">Добавить</button>' +
+    "</div>" +
+    "</div>";
+  container.insertAdjacentHTML("beforeEnd", prodact);
+}
+
+const message = document.querySelector(".error-message");
+container.addEventListener("click", (event) => {
+  if (event.target.classList.contains("add-button")) {
+    currentRecall = event.target.previousSibling.previousSibling;
+    addElement(currentRecall);
+  }
+});
+
+function addElement(currentRecall) {
+  if (
+    currentRecall.value.split("").length >= 5 &&
+    currentRecall.value.split("").length <= 500
+  ) {
+    currentIndexRecall += 1;
+    currentRecall.previousSibling.insertAdjacentHTML(
+      "afterEnd",
+      `<p>Отзыв №${currentIndexRecall}: ${currentRecall.value}</p>`
+    );
+    currentRecall.nextSibling.textContent = "";
+    for (let i = 0; i < initialData.length; i++) {
+      if (i === +currentRecall.closest(".prodact").id) {
+        initialData[i].reviews.push({
+          id: `${currentIndexRecall}`,
+          text: `${currentRecall.value}`,
+        });
+      }
+    }
+    currentRecall.value = "";
+  } else {
+    currentRecall.nextSibling.textContent =
+      "Отзыв менее 50 или более 500 символов в длину";
+  }
+}
